da5d91c6c46857e3f8143442955d12140f1e6859
Jay Patel <jaykrinapatel@gmail.com>
2025-02-23 21:50:44 -0600

    Reinitialize project

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,70 @@
+import '@fortawesome/free-regular-svg-icons';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../assets/styles/Timeline.scss';
+
+function Timeline() {
+  return (
+    <div id="history">
+      <div className="items-container">
+        <h1>Career History</h1>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="2021 - 2022"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Data Scientist Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Surat, INDIA</h4>
+            <p>
+            Full-stack Web Development, ChatBot Creation using NLP & NLU, Jenkins, Github, AWS
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2021 - 2021"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Software Developer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Vallabh-Vidyanagar, INDIA</h4>
+            <p>
+              Full Stack Development (ASP.net & C#), User Experience, IoT Softwares, CI/CD Pipelines, Github, AWS
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2020 - 2020"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Software Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Anand, INDIA</h4>
+            <p>
+              IoT Development, API Development, C++, Java Programming, Android, Testing, Github
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2019 - 2019"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">IT Support</h3>
+            <h4 className="vertical-timeline-element-subtitle">Anand, INDIA</h4>
+            <p>
+              Network, Hardware, Software, TroubleShoot
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
