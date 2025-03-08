f468c5f09291cc358ca8a867e794ec742895b867
Jay Patel <jaykrinapatel@gmail.com>
2025-03-08 15:54:16 -0600

    Configure React app for deployment to GitHub Pages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react'; // Use React Testing Library
 import App from './App';
-
+beforeEach(() => {
+	const mockIntersectionObserver = jest.fn();
+	mockIntersectionObserver.mockReturnValue({
+		observe: () => null,
+		unobserve: () => null,
+		disconnect: () => null,
+	});
+	window.IntersectionObserver = mockIntersectionObserver;
+});
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
