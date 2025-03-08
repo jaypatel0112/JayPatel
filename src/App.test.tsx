c60236cceef92e47203dd97b76f0ee4bb4178b0f
Jay Patel <jaykrinapatel@gmail.com>
2025-03-08 15:34:21 -0600

    Add GitHub Actions workflow for deployment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// Fix for the ReactDOMTestUtils.act warning
+jest.mock('react-dom', () => {
+  const original = jest.requireActual('react-dom');
+  return {
+    ...original,
+    createPortal: (node: any) => node,
+  };
+});
 
 test('renders learn react link', () => {
   render(<App />);
